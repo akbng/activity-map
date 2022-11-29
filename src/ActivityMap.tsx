f14568d5371219e7dbd3d6062ff754a2b7c33379
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-11-29 22:15:08 +0530

    add color support
    1. read proper data from the provided values
    2. color code them appropriately based on the activity/count

--- src/ActivityMap.tsx
+++ src/ActivityMap.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useMemo } from 'react';
+import Color from 'color';
 import { differenceInDays, startOfToday, subDays, subYears, addDays } from 'date-fns';
 
 export interface IActivityMap {
@@ -11,7 +12,8 @@ export interface IActivityMap {
   gutterSize?: number;
   baseColor?: string;
   weekLabelPattern?: string | string[];
-  data?: object; // TODO: describe properly
+  intensityVariance?: number;
+  values: Array<{ count: number; date: string }>; // TODO: describe properly
   // May add more properties later
 }
 
@@ -33,6 +35,34 @@ const MONTHS_OF_YEAR = [
 const daysInWeek = DAYS_OF_WEEK.length;
 const monthsInYear = MONTHS_OF_YEAR.length;
 
+export const getIntensityGroups = (intensityVariance: number, data: number[]): number[] => {
+  const uniqueValues = Array.from(new Set(data)).sort((a, b) => a - b);
+  const uniqueLength = uniqueValues.length;
+  if (uniqueLength < intensityVariance)
+    return [...new Array(intensityVariance - uniqueLength - 1).fill(0), ...uniqueValues];
+  const intensityRange = ~~(uniqueLength / (intensityVariance - 2));
+  return Array.from(
+    new Array(intensityVariance - 1),
+    (_, i) => uniqueValues[Math.min(intensityRange * i, uniqueLength - 1)]
+  );
+};
+
+const getIntensity = (
+  count: number,
+  baseColor: string,
+  intensityVariance: number,
+  intensitySteps: number[]
+) => {
+  const color = Color(baseColor);
+
+  if (count === 0) return color.lighten((intensityVariance + 1) / intensityVariance).hex();
+
+  // if (count >= Math.max(...intensitySteps)) return color.hex(); // TODO: highest intensity
+
+  const index = intensitySteps.findIndex((step) => count <= step) + 1;
+  return color.lighten((intensityVariance - index - 1) / intensityVariance).hex();
+};
+
 const ActivityMap = ({
   width,
   startDate = subYears(subDays(startOfToday(), 1), 1),
@@ -41,6 +71,8 @@ const ActivityMap = ({
   gutterSize = 3,
   cellRadius = 3,
   baseColor = '#666666',
+  values = [],
+  intensityVariance = 4,
 }: IActivityMap): ReactElement => {
   const cellSizeWithGutter = cellSize + gutterSize;
   const numOfDays = differenceInDays(endDate, startDate);
@@ -49,12 +81,38 @@ const ActivityMap = ({
   const numOfWeeks = Math.ceil((emptyDaysAtStart + numOfDays + emptyDaysAtEnd) / daysInWeek);
   const lastCellIndex = emptyDaysAtStart + numOfDays;
   const dateOfFirstCell = subDays(startDate, emptyDaysAtStart);
+  const intensitySteps = useMemo(
+    () =>
+      getIntensityGroups(
+        intensityVariance,
+        values.map((value) => value.count)
+      ),
+    [values]
+  );
+  const cellValues = useMemo<Record<number, { count: number; date: Date; intensity: string }>>(
+    () =>
+      values.reduce((acx, value) => {
+        const date = new Date(value.date);
+        const cellIndex = differenceInDays(date, dateOfFirstCell);
+        if (cellIndex < 0) return acx;
+
+        acx[cellIndex] = {
+          ...value,
+          date,
+          intensity: getIntensity(value.count, baseColor, intensityVariance, intensitySteps),
+        };
+        return acx;
+      }, Object.create(null)),
+    [values, baseColor, intensityVariance]
+  );
+
+  console.log(Color(baseColor).isLight());
 
   return (
     <svg
       width={width || cellSizeWithGutter * numOfWeeks + 40}
       viewBox={`0 0 ${cellSizeWithGutter * numOfWeeks + 40} ${
-        cellSizeWithGutter * daysInWeek + 40
+        cellSizeWithGutter * daysInWeek + 70
       }`}
     >
       <g transform={`translate(0, 40)`}>
@@ -92,12 +150,33 @@ const ActivityMap = ({
                   width={cellSize}
                   height={cellSize}
                   rx={cellRadius}
-                  fill={baseColor}
+                  fill={cellValues[cellIndex]?.intensity || Color(baseColor).lighten(0.75).hex()}
                 />
               );
             })}
           </g>
         ))}
+      </g>
+      <g transform={`translate(40, ${cellSizeWithGutter * daysInWeek + 50})`}>
+        <text x={0} y={cellSize}>
+          More
+        </text>
+        {Array.from(new Array(intensityVariance), (_, i) => i).map((variance) => (
+          <rect
+            key={variance}
+            x={variance * cellSizeWithGutter + 40}
+            y={0}
+            width={cellSize}
+            height={cellSize}
+            rx={cellRadius}
+            fill={Color(baseColor)
+              .lighten(variance / intensityVariance)
+              .hex()}
+          />
+        ))}
+        <text x={intensityVariance * cellSizeWithGutter + 40} y={cellSize}>
+          Less
+        </text>
       </g>
     </svg>
   );
