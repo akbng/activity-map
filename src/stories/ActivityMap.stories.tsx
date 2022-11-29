d2f2e4b63b75554877d06f841a4a588ff7cd377e
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-11-29 22:43:46 +0530

    add randomly generated data for story component

--- src/stories/ActivityMap.stories.tsx
+++ src/stories/ActivityMap.stories.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import * as sb from '@storybook/react';
+import { addDays, differenceInDays, format, startOfToday, subYears } from 'date-fns';
 
 import ActivityMap from '../ActivityMap';
+
+const generateRandomNumbers = (min = 0, max = 100) => Math.round(Math.random() * (max - min) + min);
+
+const generateData = (startDate: Date, endDate: Date): Array<{ date: string; count: number }> =>
+  new Array(differenceInDays(endDate, startDate)).fill(0).map((_, i) => ({
+    date: format(addDays(startDate, i), 'yyyy-MM-dd'),
+    count: generateRandomNumbers(0, 25),
+  }));
 
 export default {
   title: 'ActivityMap',
@@ -12,3 +21,7 @@ export default {
 const Template: sb.ComponentStory<typeof ActivityMap> = (args) => <ActivityMap {...args} />;
 
 export const Primary = Template.bind({});
+Primary.args = {
+  values: generateData(subYears(startOfToday(), 1), startOfToday()),
+  baseColor: '#ff0000',
+};
