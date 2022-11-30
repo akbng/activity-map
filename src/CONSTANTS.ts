27fb989811dbf6b64ea28b35f6bc922cd0a8acb8
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-11-30 17:37:11 +0530

    add constants file

---
+++ src/CONSTANTS.ts
@@ -0,0 +1,24 @@
+export const DAYS_OF_WEEK = [
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+];
+
+export const MONTHS_OF_YEAR = [
+  'january',
+  'february',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december',
+];
